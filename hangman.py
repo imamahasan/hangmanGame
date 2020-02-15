7bcdf73c5334c1eaac2bd88df5bc7115dc4843ed
imamahasan <57554932+imamahasan@users.noreply.github.com>
2020-02-16 01:59:37 +0600

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,33 +1,24 @@
 
 import random
 
-#word_file_name = "words.txt"
-#
-#
-#def loadWords():
-##    print("Loading Word List From File.")
-#    
-#    in_file = open(word_file_name)
-#    line = in_file.readline()
-#    word_list = line.split()
-#    
-#    print(" ", len(word_list), "Words Loaded.")
-##    print(word_list)
-#    
-#    return word_list
 line = ['rainbow', 'computer', 'science', 'programming',  
          'python', 'mathematics', 'player', 'condition',  
-         'reverse', 'water', 'board', 'imam'] 
+         'reverse', 'water', 'board', 'imam', 'samiul', 
+         'noor', 'google', 'daffodil', 'dhaka', 'ahasan'] 
 loadWords = random.choice(line)
 
-#def chooseWord(word_list):   
-#    return random.choice(word_list)
+def wordGuessed(secretWord, lettersGuessed):
+    count=0
+    for letters in secretWord:
+        if letters in lettersGuessed:
+            count+=1
+    if count==len(secretWord):
+        return True
+    else:
+        return False
+    
 
-
-#word_list = loadWords()
-
-
-def word_Guess(secretWord, letterGuess):
+def getWord_Guess(secretWord, letterGuess):
     string = ""
     
     for key in secretWord:
@@ -64,36 +55,34 @@ def hangman(secretWord):
     letterGuess = []
     
     while (chances != 0):
-#        print("----------")
         
-        if secretWord != word_Guess(secretWord, letterGuess):
+        if secretWord != getWord_Guess(secretWord, letterGuess):
             print("\n\tYou have", chances, "guesses left")
-#            print("Available letters: ", getAvailabeleLetters(letterGuess))
             
             guess = input("Please guess a letter: ")
             guessLowerCase = guess.lower()
             
             if guessLowerCase in letterGuess:
-                print("You Have already guessed that letter: ", word_Guess(secretWord, letterGuess))
+                print("You Have already guessed that letter: ", getWord_Guess(secretWord, letterGuess))
                 
             elif guessLowerCase not in secretWord:
-                print("That letter is not in my word: ", word_Guess(secretWord, letterGuess))
+                print("That letter is not in my word: ", getWord_Guess(secretWord, letterGuess))
                 chances -= 1
                 
             else:
                 letterGuess.append(guessLowerCase)
-                print("Good Guess: ", word_Guess(secretWord, letterGuess))
+                print("Good Guess: ", getWord_Guess(secretWord, letterGuess))
                 
             letterGuess.append(guessLowerCase)
             
-        elif secretWord == word_Guess(secretWord, letterGuess):
+        elif secretWord == getWord_Guess(secretWord, letterGuess):
             print("Congratulations, You Win!")
             break
         
     else:
-        print("----------")
         print("Sorry, You ran out. The word was \'" + secretWord + "\'.")
         
 secretWord = loadWords
 hangman(secretWord)
-     
+            
+    
